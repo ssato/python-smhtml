e8af8c42c02f7522b5f6632ed08557bca9bf7233
Satoru SATOH <satoru.satoh@gmail.com>
2019-02-25 17:06:49 +0900

    fix: [doc] update the smhtml module description

--- smhtml/__init__.py
+++ smhtml/__init__.py
@@ -5,12 +5,15 @@
 r"""
 .. module:: smhtml
    :platform: Unix, Windows
-   :synopsis: Simple MHTML parsing library
+   :synopsis: Simple python library to load, extract and dump MHTML data
 
-python-smhtml is a simple and experimental MHTML parsing library for python.
+python-smhtml is a simple and experimental python library to load MHTML files and
+extract files from them, and dump (make) MHTML data from files.
 
 - Home: https://github.com/ssato/python-smhtml
 
+About MHTML format, please refer other web pages such like
+https://en.wikipedia.org/wiki/MHTML.
 """
 from .api import (
     AUTHOR, VERSION, load, loads, dump, dumps, extract  # flake8: noqa
